7f1d297b6836661670068984a447336ac21804d5
Ali Yiğit Yılmaz <yigit_yilmaz2003@hotmail.com>
2025-03-13 02:27:51 +0300

    Interactable Class and PlayerController Implementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using System.Linq; // En yakýn nesneyi bulmak için
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float interactionRadius = 2f; // Etkileþim yarýçapý
+
     private Vector3 moveDirection;
 
     void Update()
     {
         HandleMovement();
+        HandleInteraction();
     }
 
     void HandleMovement()
@@ -25,5 +29,45 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.position += isoDirection * moveSpeed * Time.deltaTime;
+    }
+
+    void HandleInteraction()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) // 'E' tuþuna basýldýðýnda
+        {
+            Interactable nearestInteractable = FindNearestInteractable();
+            if (nearestInteractable != null)
+            {
+                nearestInteractable.Interact(); // En yakýn objeyle etkileþim
+            }
+        }
+    }
+
+    Interactable FindNearestInteractable()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, interactionRadius);
+        Interactable nearest = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (Collider col in colliders)
+        {
+            Interactable interactable = col.GetComponent<Interactable>();
+            if (interactable != null)
+            {
+                float distance = Vector3.Distance(transform.position, col.transform.position);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    nearest = interactable;
+                }
+            }
+        }
+        return nearest;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, interactionRadius);
     }
 }
