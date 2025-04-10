8747fc92fe2491429a8e196406acdbae360f01bb
Cihan <nakpinar@cxuni.com>
2025-04-10 18:37:25 +0300

    HeroManager sahne otomatik list doldurma

--- Assets/Scripts/Player/HeroManager.cs
+++ Assets/Scripts/Player/HeroManager.cs
@@ -1,25 +1,60 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; using UnityEngine.SceneManagement; using System.Collections.Generic;
 
 public class HeroManager : MonoBehaviour
-{
-    
+{ 
     public static HeroManager instance;
     public List<Hero> heroList = new List<Hero>();
-
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
+            // Sahne yüklendiğinde listemizi güncellemek için event'e abone oluyoruz
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else
         {
-                Destroy(gameObject);
+            Destroy(gameObject);
         }
     }
-    
-    
+
+    private void OnDestroy()
+    {
+        // Bellek sızıntısını önlemek için event aboneliğini kaldırıyoruz
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+// Yeni bir sahne yüklendiğinde çağrılır
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        PopulateHeroList();
+    }
+
+// Sahnedeki tüm HeroTargetable ve EnemyTargetable bileşenlerini bularak heroList'e ekler
+    private void PopulateHeroList()
+    {
+        // Önce listede önceki sahneden kalan verileri temizleyin
+        heroList.Clear();
+
+        // Sahnedeki tüm HeroTargetable nesnelerinden verileri al
+        HeroTargetable[] heroTargets = GameObject.FindObjectsOfType<HeroTargetable>();
+        foreach (var ht in heroTargets)
+        {
+            if (ht.heroData != null)
+            {
+                heroList.Add(ht.heroData);
+            }
+        }
+
+        // Sahnedeki tüm EnemyTargetable nesnelerinden verileri al
+        EnemyTargetable[] enemyTargets = GameObject.FindObjectsOfType<EnemyTargetable>();
+        foreach (var et in enemyTargets)
+        {
+            if (et.enemyData != null)
+            {
+                heroList.Add(et.enemyData);
+            }
+        }
+    }
 }
