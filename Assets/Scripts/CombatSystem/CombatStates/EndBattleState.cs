dab58ce48c7cd89870d27a2829dc9fcd5808b47d
LariXv1 <berkaysagdic@hotmail.com>
2025-05-22 21:18:55 +0300

    State geçişleri arasına cooldown eklendi. Canı biten karakterlerin ölmesi ve bir takımdaki herkes ölünce oyunun bitmesi yazıldı.

---
+++ Assets/Scripts/CombatSystem/CombatStates/EndBattleState.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class EndBattleState : ICombatState
+{
+    private CombatStateManager manager;
+    public EndBattleState(CombatStateManager manager)
+    {
+        this.manager = manager;
+    }
+    public void Enter()
+    {
+
+    }
+
+    public void Execute()
+    {
+       
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
--- Assets/Scripts/Player/HeroTargetable.cs
+++ Assets/Scripts/Player/HeroTargetable.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class HeroTargetable : MonoBehaviour
 {
     public Hero heroData;
-
+    private Collider myCollider;
+    private void Awake()
+    {
+        myCollider = GetComponent<Collider>();
+    }
     private void OnMouseDown()
     {
         if (CombatStateManager.Instance.IsTargetSelectionActive)
@@ -17,4 +21,21 @@ public class HeroTargetable : MonoBehaviour
             }
         }
     }
+    public void Die()
+    {
+        Debug.Log($"{heroData.name} died.");
+
+        // 1. HeroManager listesinden çýkar
+        HeroManager.instance.heroList.Remove(heroData);
+
+        CombatStateManager.Instance.RemoveFromTurnOrder(heroData);
+
+        if (myCollider != null)
+            myCollider.enabled = false;
+
+        gameObject.SetActive(false);
+
+        CombatStateManager.Instance.CheckBattleEnd();
+    }
 }
+
