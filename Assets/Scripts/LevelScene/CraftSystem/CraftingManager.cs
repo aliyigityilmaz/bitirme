84f82f5efb1c3adede0639e3f9927b32429c6b9d
Ali Yiğit Yılmaz <yigit_yilmaz2003@hotmail.com>
2025-06-09 12:23:30 +0200

    crafting system tamamlandı

--- Assets/Scripts/LevelScene/CraftSystem/CraftingManager.cs
+++ Assets/Scripts/LevelScene/CraftSystem/CraftingManager.cs
@@ -5,22 +5,43 @@ using System.Collections.Generic;
 
 public class CraftingManager : MonoBehaviour
 {
-    public Transform recipeListParent;        // Sol sütun
-    public GameObject recipeButtonPrefab;     // Prefab: isim + icon
-    public GameObject ingredientRowPrefab;    // Saðdaki malzeme listesi (icon + adet)
 
-    public Image resultIcon;
-    public TextMeshProUGUI resultName;
-    public Button craftButton;
-    public Transform ingredientParent;
-
+    [Header("Sol Liste")]
+    public Transform recipeListParent;
+    public GameObject recipeButtonPrefab;
     public List<RecipeData> recipes;
 
+    [Header("Sað Panel")]
+    public Image resultIcon;
+    public TextMeshProUGUI resultName;
+
+    [Header("Ingredient 1")]
+    public Image ingredient1Icon;
+    public TextMeshProUGUI ingredient1Count;
+
+    [Header("Ingredient 2")]
+    public Image ingredient2Icon;
+    public TextMeshProUGUI ingredient2Count;
+
+    [Header("Craft")]
+    public Button craftButton;
+
+    [Header("Sprites")]
+    public Sprite CharacterItemBackground;
+    public Sprite QuestItemBackground;
+    public Sprite CollectibleItemBackground;
+    public Sprite CharacterItemBackgroundSelected;
+    public Sprite QuestItemBackgroundSelected;
+    public Sprite CollectibleItemBackgroundSelected;
+
     private RecipeData selectedRecipe;
+    private GameObject selectedButton;
 
     private void Start()
     {
         PopulateRecipeList();
+        craftButton.onClick.AddListener(CraftItem); // Tek seferlik baðlanýr
+        resultName.text = " ";
     }
 
     void PopulateRecipeList()
@@ -34,55 +55,132 @@ public class CraftingManager : MonoBehaviour
             go.transform.Find("Icon").GetComponent<Image>().sprite = recipe.resultIcon;
             go.transform.Find("Name").GetComponent<TextMeshProUGUI>().text = recipe.resultItemName;
 
+            Image bgImage = go.GetComponent<Image>();
+            if (recipe.resultItem.type == ItemType.QuestItem)
+                bgImage.sprite = QuestItemBackground;
+            else if (recipe.resultItem.type == ItemType.CharacterItem)
+                bgImage.sprite = CharacterItemBackground;
+            else if (recipe.resultItem.type == ItemType.Collectible)
+                bgImage.sprite = CollectibleItemBackground;
+
             var btn = go.GetComponent<Button>();
-            btn.onClick.AddListener(() => ShowRecipe(recipe));
+            btn.onClick.AddListener(() => ShowRecipe(recipe, go)); // Butonu da gönderiyoruz
         }
     }
 
-    void ShowRecipe(RecipeData recipe)
+    void ShowRecipe(RecipeData recipe, GameObject buttonGO)
     {
+        
         selectedRecipe = recipe;
+
+        if (selectedButton != null)
+        {
+            RecipeData prevRecipe = recipes.Find(r => r == selectedRecipe);
+            Image prevImage = selectedButton.GetComponent<Image>();
+
+            if (prevRecipe.resultItem.type == ItemType.QuestItem)
+                prevImage.sprite = QuestItemBackground;
+            else if (prevRecipe.resultItem.type == ItemType.CharacterItem)
+                prevImage.sprite = CharacterItemBackground;
+            else if (prevRecipe.resultItem.type == ItemType.Collectible)
+                prevImage.sprite = CollectibleItemBackground;
+            selectedButton.transform.Find("Name").GetComponent<TextMeshProUGUI>().color = Color.white;
+        }
+
+        Image newImage = buttonGO.GetComponent<Image>();
+        if (recipe.resultItem.type == ItemType.QuestItem)
+            newImage.sprite = QuestItemBackgroundSelected;
+        else if (recipe.resultItem.type == ItemType.CharacterItem)
+            newImage.sprite = CharacterItemBackgroundSelected;
+        else if (recipe.resultItem.type == ItemType.Collectible)
+            newImage.sprite = CollectibleItemBackgroundSelected;
+        selectedButton = buttonGO;
+        selectedButton.transform.Find("Name").GetComponent<TextMeshProUGUI>().color = Color.black;
 
         resultIcon.sprite = recipe.resultIcon;
         resultName.text = recipe.resultItemName;
 
-        foreach (Transform child in ingredientParent)
-            Destroy(child.gameObject);
+       
 
+        // Ingredient 1
+        if (recipe.ingredients.Length > 0)
+        {
+            ingredient1Icon.sprite = recipe.ingredients[0].item.icon;
+            ingredient1Count.text = "x" + recipe.ingredients[0].quantity;
+            ingredient1Count.gameObject.SetActive(true);
+            ingredient1Icon.gameObject.SetActive(true);
+        }
+        else
+        {
+
+            ingredient1Count.gameObject.SetActive(false);
+            ingredient1Icon.gameObject.SetActive(false);
+        }
+
+        // Ingredient 2
+        if (recipe.ingredients.Length > 1)
+        {
+            ingredient2Icon.sprite = recipe.ingredients[1].item.icon;
+            ingredient2Count.text = "x" + recipe.ingredients[1].quantity;
+            ingredient2Count.gameObject.SetActive(true);
+            ingredient2Icon.gameObject.SetActive(true);
+        }
+        else
+        {
+
+            ingredient2Count.gameObject.SetActive(false);
+            ingredient2Icon.gameObject.SetActive(false);
+        }
+
+        // Craft tuþu aktif mi?
         bool canCraft = true;
-
         foreach (var ing in recipe.ingredients)
         {
-            GameObject row = Instantiate(ingredientRowPrefab, ingredientParent);
-            row.transform.Find("Icon").GetComponent<Image>().sprite = ing.item.icon;
-            row.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = $"{ing.item.itemName} x{ing.quantity}";
-
-            if (!BackpackManager.Instance.CheckItem(ing.item.itemName)) // sadece varlýk kontrolü
+            int playerCount = BackpackManager.Instance.GetItemCount(ing.item);
+            if (playerCount < ing.quantity)
                 canCraft = false;
         }
 
         craftButton.interactable = canCraft;
-        craftButton.onClick.RemoveAllListeners();
-        craftButton.onClick.AddListener(() => CraftItem());
     }
 
     void CraftItem()
     {
-        if (selectedRecipe == null) return;
+        if (selectedRecipe == null)
+        {
+            Debug.LogWarning("Hiçbir tarif seçilmedi.");
+            return;
+        }
 
+        // Malzeme kontrolü
+        foreach (var ing in selectedRecipe.ingredients)
+        {
+            if (BackpackManager.Instance.GetItemCount(ing.item) < ing.quantity)
+            {
+                Debug.LogWarning("Craft baþarýsýz, yeterli malzeme yok.");
+                return;
+            }
+        }
+
+        // Malzemeleri sil
         foreach (var ing in selectedRecipe.ingredients)
         {
             for (int i = 0; i < ing.quantity; i++)
             {
-                if (!BackpackManager.Instance.RemoveItem(ing.item.itemName))
+                if (!BackpackManager.Instance.RemoveItem(ing.item))
                 {
-                    Debug.LogWarning("Craft iþlemi yarýda kaldý, yeterli malzeme yok!");
+                    Debug.LogWarning("Beklenmeyen hata: Malzeme silinemedi.");
                     return;
                 }
             }
         }
 
+        // Üretilen itemi envantere ekle
         BackpackManager.Instance.AddItem(selectedRecipe.resultItem);
-        ShowRecipe(selectedRecipe); // UI'yý güncelle
+
+        // UI’ý güncelle (craft tuþu aktif/pasif vs.)
+        ShowRecipe(selectedRecipe, selectedButton);
+
     }
+
 }
